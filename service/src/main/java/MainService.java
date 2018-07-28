f8070c9d4f456a0d8aa40ae69c609929e609db45
j585zhan <j585zhan@uwaterloo.ca>
2018-07-28 19:34:07 -0400

    update gitignore

--- service/src/main/java/MainService.java
+++ service/src/main/java/MainService.java
@@ -2,6 +2,7 @@ import types.Player;
 
 public class MainService {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        final Player p = new Player().withAlive(false).withName("Tester").withId(123);
+        System.out.println("Hello World, " + p.getName());
     }
 }
