672010ff8640ef886599b8f21f5add35162c3e80
j585zhan <j585zhan@uwaterloo.ca>
2018-07-29 18:59:33 -0400

    update types. load script from json and convert to java class.

--- service/src/main/java/MainService.java
+++ service/src/main/java/MainService.java
@@ -1,8 +1,29 @@
-import types.Player;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.apache.commons.io.IOUtils;
+import types.Script;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainService {
-    public static void main(String[] args) {
-        final Player p = new Player().withAlive(false).withName("Tester").withId(123);
-        System.out.println("Hello World, " + p.getName());
+    public static void main(String[] args) throws IOException {
+        InputStream is = new FileInputStream("script.json");
+        String jsonTxt = IOUtils.toString(is, "UTF-8");
+        JsonObject json = (JsonObject) new JsonParser().parse(jsonTxt);
+
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        Gson gson = gsonBuilder.create();
+
+        Script script = gson.fromJson(json, Script.class);
+        for (String s: script.getPlayers().get(0).getMission()) {
+            System.out.println(s);
+        }
+
+        System.out.println(script.getNpcs().get(0).getDescription());
+
     }
 }
