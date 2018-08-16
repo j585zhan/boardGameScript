639409497490bf17c4f2b64ff9576299824e40c5
j585zhan <j585zhan@uwaterloo.ca>
2018-08-16 17:28:39 -0400

    CORS handler for pre-flight

--- service/src/main/java/web/RouteBuilder.java
+++ service/src/main/java/web/RouteBuilder.java
@@ -2,10 +2,18 @@ package web;
 
 import action.ActionHandler;
 import action.GetScriptHandler;
+import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.CorsHandler;
 import lombok.Getter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 public class RouteBuilder {
@@ -16,10 +24,30 @@ public class RouteBuilder {
         router = Router.router(vertx);
         router.route().handler(BodyHandler.create());
         Route("/get", new GetScriptHandler());
+
+        router.route().handler(GetCORSHandler());
     }
 
     private <Req, Resp> void Route(final String path, final ActionHandler<Req, Resp> action) {
         WebHandler<Req, Resp> webHandler = new WebHandler<>(action);
+        router.route(path).handler(GetCORSHandler());
         router.route(path).handler(webHandler::handle);
+    }
+
+    private static Handler<RoutingContext> GetCORSHandler() {
+        final Set<String> allowedHeaders = new HashSet<>();
+        allowedHeaders.add(HttpHeaders.CONTENT_TYPE.toString());
+        allowedHeaders.add(HttpHeaders.ACCEPT.toString());
+        allowedHeaders.add(HttpHeaders.AUTHORIZATION.toString());
+
+        final Set<HttpMethod> allowedMethods = new HashSet<>();
+        allowedMethods.add(HttpMethod.POST);
+        allowedMethods.add(HttpMethod.OPTIONS);
+
+        final CorsHandler handler = CorsHandler
+            .create("*")
+            .allowedHeaders(allowedHeaders)
+            .allowedMethods(allowedMethods);
+        return handler;
     }
 }
